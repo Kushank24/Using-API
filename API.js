dac7490ea44d6818a6251965977f9510e613f0eb
Abhishek2022 <avishekprasad26@gmail.com>
2020-07-06 00:05:25 +0530

    new api added

--- API.js
+++ API.js
@@ -1,7 +1,6 @@
 const url = "http://api.weatherstack.com/current?access_key=dc9867c8d42970bf5290b8a660957fc0&query=New%20York";
 const content = document.getElementById('content')
 
-console.log(content)
 function getdata() {
   fetch(url).then(data => data.json()).then((data) => (content.textContent = data.current.temperature));
 }
